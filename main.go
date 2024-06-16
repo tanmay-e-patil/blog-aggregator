674312a2a594c28cdbaffd227f8061a8835d2346
Tanmay Patil <patil.tanmay0697@gmail.com>
2024-06-16 14:12:21 -0700

    Add some basic json handlers

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
+)
 
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+		return
+	}
+	port := os.Getenv("PORT")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthz", handlerReadiness)
+	mux.HandleFunc("/v1/err", handlerErr)
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(srv.ListenAndServe())
 }
